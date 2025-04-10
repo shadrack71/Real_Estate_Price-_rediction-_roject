e0312825f831b099be89a4e511896fc535e0c086
shadrack <shadrackkinsimba@gmail.com>
2025-04-10 12:46:18 +0300

    build flask server , run ML model

---
+++ web_app/server.py
@@ -0,0 +1,38 @@
+from flask import Flask , request,jsonify,render_template
+import util
+
+app = Flask(__name__, template_folder='templates',static_folder='static',static_url_path='/')
+
+util.load_saved_artifacts()
+
+@app.route('/')
+def homepage():
+   return render_template('index.html')
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
